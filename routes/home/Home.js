1f5148095cf2a5c780b9b6b7463dc657219ad44d
Traceton Timmerman <traceton.timmerman@gmail.com>
2020-12-09 20:05:57 -0500

    initial react-navigation creation

--- routes/home/Home.js
+++ routes/home/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import Home from "./routes/home/Home";
+import Home from "./routes/Home";
 
 const Tab = createBottomTabNavigator();
 export default function App() {
