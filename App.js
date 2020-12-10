63f22f5e3d5696410ea7aabc91d57c583222f3b8
Traceton Timmerman <traceton.timmerman@gmail.com>
2020-12-09 20:17:17 -0500

    navigation changes

--- App.js
+++ App.js
@@ -3,14 +3,13 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import Home from "./routes/home/Home";
-
+import HomeStackManager from "./routes/home/HomeStackManager";
 const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Home" component={HomeStackManager} />
       </Tab.Navigator>
     </NavigationContainer>
   );
