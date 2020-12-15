03d8167e8f3e6042e2b68863e9e8837640d804be
Traceton Timmerman <traceton.timmerman@gmail.com>
2020-12-15 15:20:14 -0500

    voting poll preview small change

--- routes/vote/VotingPoll.js
+++ routes/vote/VotingPoll.js
@@ -2,9 +2,11 @@ import React from "react";
 import { View, Text } from "react-native";
 import { Button, Card } from "react-native-elements";
 export default function VotingPoll({ navigation }) {
-  return (
+  let displayedVotingPoll = (
     <Card>
       <Card.Title>voting poll card title</Card.Title>
     </Card>
   );
+
+  return { displayedVotingPoll };
 }
--- routes/vote/VotingPollPreview.js
+++ routes/vote/VotingPollPreview.js
@@ -2,9 +2,27 @@ import React from "react";
 import { View, Text } from "react-native";
 import { Button, Card } from "react-native-elements";
 export default function VotingPollPreview({ navigation }) {
-  return (
-    <Card>
-      <Card.Title>voting poll preview card title</Card.Title>
-    </Card>
-  );
+  let displayedVotingPollPreview;
+
+  let pollType = "singleQuestion";
+
+  if (pollType === "singleQuestion") {
+    displayedVotingPollPreview = (
+      <Card>
+        <Card.Title>
+          multiple question poll. voting poll preview card title
+        </Card.Title>
+      </Card>
+    );
+  } else if (pollType === "multipleQuestion") {
+    displayedVotingPollPreview = (
+      <Card>
+        <Card.Title>
+          single question poll. voting poll preview card title
+        </Card.Title>
+      </Card>
+    );
+  }
+
+  return <View>{displayedVotingPollPreview}</View>;
 }
